3d1de5398d6124f40e1ab9de543fdfd004ab761d
Maghion <geo.f.medina@gmail.com>
2020-01-14 10:46:31 -0500

    we finally done it

--- scripts/store.js
+++ scripts/store.js
@@ -4,7 +4,7 @@ const items = [];
 let hideCheckedItems = false;
 
 const findById = function (id) {
-  return this.items.find(items => items.id === id).name;
+  return items.find(items => items.id === id);
 };
 
 const addItem = function (name) {
@@ -18,31 +18,42 @@ const addItem = function (name) {
 };
 
 const findAndToggleChecked = function (id) {
-  let toToggle = findById(id).indexOf();
-  if (this.items[toToggle].checked === false) {
-    this.items[toToggle].checked = true; 
+  const index = this.items.indexOf(findById(id));
+  if (this.items[index].checked === false) {
+    this.items[index].checked = true; 
   } else {
-    this.items[toToggle].checked = false;
+    this.items[index].checked = false;
   }
 };
 
 const findAndUpdateName = function(id, newName) {
   try {
-    item.validateName(name);
-    findById();
-    
-    ;
+    item.validateName(newName);
+    const index = this.items.indexOf(findById(id));
+    this.items[index].name = newName;
   }
   catch (e) {
     console.log( `Cannot update name: ${e.message}`);
   }
 };
 
-const findAndD
+const findAndDelete = function (id) {
+  const index = this.items.indexOf(findById(id));
+  this.items.splice(index, 1);
+};
 
+const toggleCheckedFilter = function() {
+  this.hideCheckedItems = !this.hideCheckedItems;
+};
 
   
 export default {
   items,
-  hideCheckedItems
+  hideCheckedItems,
+  findById,
+  addItem,
+  findAndToggleChecked,
+  findAndUpdateName,
+  findAndDelete,
+  toggleCheckedFilter
 };
